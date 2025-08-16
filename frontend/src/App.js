1b7500a0b3fd27842c71b15d3fde40577db98443
Him Kishan Das <70309670+Him-Kishan-Das@users.noreply.github.com>
2025-08-17 01:54:29 +0530

    feat: our services

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import AnimatedBackground from "./components/AnimatedBackground"; 
 import Footer from "./components/Footer";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Navbar />
       <Hero />
       <Highlights />
+      <Services />
       <Features />
       <About />
       <Contact />
